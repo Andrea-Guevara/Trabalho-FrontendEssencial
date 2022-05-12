d7237ca7904bc83386397728546ed873e2e10cc1
Andrea-Guevara <montoyaandrea2018@gmail.com>
2022-05-12 15:27:58 -0300

    modificacao script

--- script2.js
+++ script2.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-    fetch("https://randomuser.me/api/?results=9").then(resultado => {
+    fetch("https://randomuser.me/api/?results=5").then(resultado => {
        return resultado.json()
    }).then(response => {
-       for (i = 0; i < 9; i++) {           
+       for (i = 0; i < 5; i++) {           
            document.getElementById("foto" + i).setAttribute("src", response.results[i].picture.large);
            document.getElementById("nome" + i).innerHTML = response.results[i].name.first + " " + response.results[i].name.last;
            document.getElementById("localizacao" + i).innerHTML = response.results[i].location.timezone.description;               
